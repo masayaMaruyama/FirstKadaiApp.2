1ac73d124f2fb7208eba83813d3560b04677475d
masaya.maruyama <yayewanshan2@gmail.com>
2021-12-09 19:52:56 +0900

    Initial Commit

---
+++ FirstKadaiApp.2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirstKadaiApp.2
+//
+//  Created by jobs steve on 2021/12/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
